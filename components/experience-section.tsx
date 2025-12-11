b80724eb614a10de909da415d77c5d5697b53d53
Sanketh <sankethsequeira@gmail.com>
2025-12-11 02:36:14 -0600

    changes to experience

--- components/experience-section.tsx
+++ components/experience-section.tsx
@@ -9,7 +9,7 @@ export function ExperienceSection() {
       company: "UIC - School Of Public Health",
       location: "Chicago, Illinois, USA",
       duration: "December 2025 - Present",
-      description: "Developing and maintaining web applications for the School of Public Health.",
+      description: "",
       achievements: [
         "Researching, designing, and developing .NET web features and SQL database components to support ongoing projects.",
         "Maintaining and improving the existing multi-platform websites and applications through fixes and feature improvements",
